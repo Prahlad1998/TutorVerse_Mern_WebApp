f05e88b57fda5add9d01a823adef2f559ae3c887
Prahlad1998 <76807359+Prahlad1998@users.noreply.github.com>
2023-08-03 16:57:27 +0530

    changes in mail settings

--- backend/middleware/sendEmailMiddleware.js
+++ backend/middleware/sendEmailMiddleware.js
@@ -17,7 +17,8 @@ const sendOTPmiddleware = async (req, res, next) => {
     let transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: true,
+      secure: false,
+      requireTLS:true,
       auth: {
         user: process.env.SMTP_MAIL,
         pass: process.env.SMTP_PASS,
@@ -72,7 +73,8 @@ const sendOTPmiddlewareStudent = async (req, res, next) => {
     let transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: true,
+      secure: false,
+      requireTLS:true,
       auth: {
         user: process.env.SMTP_MAIL,
         pass: process.env.SMTP_PASS,
