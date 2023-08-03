93598ab2d51495e340e98f0bd4239e2ddb08ba0a
Prahlad1998 <76807359+Prahlad1998@users.noreply.github.com>
2023-08-03 17:46:04 +0530

    change in react comp

--- test/src/App.js
+++ test/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
